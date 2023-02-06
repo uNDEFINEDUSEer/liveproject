5005b15661f7099fb0a37c05488f955f32d963ea
uNDEFINEDUSEer <120769778+uNDEFINEDUSEer@users.noreply.github.com>
2023-02-06 12:28:28 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = "sql12.freesqldatabase.com";
-$user = "sql12595992";
-$password = "piyushdatabase";
+$host = "sql14.mysqldatabase.com";
+$user = "sql9366803";
+$password = "yahipasswordhai";
 $db_name = "sql12595992";
 $clean = mysqli_connect($host, $user, $password, $db_name,);
 if (mysqli_connect_errno()) {
